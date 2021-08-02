f9ee5d8f4e14a78d051b540745258735ba60b0de
Art <a.muhrin@yandex.ru>
2021-08-02 16:15:16 +0300

    .

--- src/main/java/com/example/keyboardservice/controller/AppController.java
+++ src/main/java/com/example/keyboardservice/controller/AppController.java
@@ -6,10 +6,14 @@ import com.example.keyboardservice.service.KeyService;
 import com.example.keyboardservice.service.KeyboardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("keyboard")
@@ -29,9 +33,9 @@ public class AppController {
         return ResponseEntity.ok(keyboardService.getKeyboard());
     }
 
-    @GetMapping("/key")
+    @PostMapping("/key")
     @ResponseBody
-    public ResponseEntity<Message> handleKey(@RequestParam Message message) {
+    public ResponseEntity<Message> handleKey(@RequestBody Message message) {
         return ResponseEntity.ok(keyService.handleMessage(message));
     }
 }
